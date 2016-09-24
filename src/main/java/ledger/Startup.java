b3faa86a2a4898f3022dd7e1e2f93d0e6e9e4058
CJ Miller <cjmiller721@gmail.com>
2016-09-24 19:02:43 -0400

    [LED-33] Adding Base Branch with file structure.

---
+++ src/main/java/ledger/Startup.java
@@ -0,0 +1,11 @@
+package ledger;
+
+/**
+ * Created by CJ on 9/24/2016.
+ */
+public class Startup {
+
+    public static void main(String[] args) {
+        System.out.println("Applicaiton has compiled and ran.");
+    }
+}
---
+++ src/test/java/ledger/StartupTest.java
@@ -0,0 +1,9 @@
+package ledger;
+
+/**
+ * Created by CJ on 9/24/2016.
+ */
+
+// Base Class
+public class StartupTest {
+}
