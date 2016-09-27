f4515b87297714995679f11787ecbb98cadb5fec
Tayler How <taylerhow@gmail.com>
2016-09-26 20:47:17 -0400

    [LED-39] Added functions to the IDatabase interface

--- src/main/java/ledger/database/IDatabase.java
+++ src/main/java/ledger/database/IDatabase.java
@@ -1,7 +1,43 @@
 package ledger.database;
 
+import ledger.database.enity.Transaction;
+
 /**
  * Responsible for Physical Storage Mechanisms
  */
 public interface IDatabase {
+
+    void initializeDatabase();
+
+    void openDatabase();
+
+    void closeDatabase();
+
+    void wipeDatabase();
+
+    void shutdown();
+
+    void insertTransaction(Transaction transaction);
+
+    void deleteTransaciton(Transaction transaction);
+
+    void editTransaction(Transaction transaction);
+
+    void insertAccount(Account account);
+
+    void deleteAccount(Account account);
+
+    void editAccount(Account account);
+
+    void insertPayee(Payee payee);
+
+    void deletePayee(Payee payee);
+
+    void editPayee(Payee payee);
+
+    void insertType(Type type);
+
+    void deleteType(Type type);
+
+    void editType(Type type);
 }
