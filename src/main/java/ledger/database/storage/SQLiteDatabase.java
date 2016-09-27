df0187984158db10a906f72638a8f33ab5514823
Tayler How <taylerhow@gmail.com>
2016-09-27 17:26:44 -0400

    [LED-39] Finished implementing base code for transaction functions, still missing helper functions. Logic needs to be reviewed.

--- src/main/java/ledger/database/storage/SQLiteDatabase.java
+++ src/main/java/ledger/database/storage/SQLiteDatabase.java
@@ -2,10 +2,14 @@ package ledger.database.storage;
 
 import ledger.database.IDatabase;
 import ledger.database.enity.*;
+
 import java.io.InputStream;
 import java.sql.*;
-import java.util.*;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+import java.
 
 /**
  * Database handler for SQLite storage mechanism.
@@ -48,7 +52,7 @@ public class SQLiteDatabase implements IDatabase {
         tableSQL.add(tableTagToPayee);
 
         try {
-            for(String statement: tableSQL) {
+            for (String statement : tableSQL) {
                 Statement stmt = database.createStatement();
                 stmt.execute(statement);
                 stmt.close();
@@ -73,7 +77,7 @@ public class SQLiteDatabase implements IDatabase {
             "NOTE_TEXT TEXT             NOT NULL, " +
             "FOREIGN KEY(NOTE_TRANS_ID) REFERENCES TRANSACTION(TRANS_ID)" +
             ")";
-    private final String tableTagToTrans= "CREATE TABLE IF NOT EXISTS TAG_TO_TRANS " +
+    private final String tableTagToTrans = "CREATE TABLE IF NOT EXISTS TAG_TO_TRANS " +
             "(TTTS_TAG_ID INT           NOT NULL," +
             "TTTS_TRANS_ID" +
             "FOREIGN KEY(TTTS_TAG_ID) REFERENCES TAG(TAG_ID), " +
@@ -84,35 +88,34 @@ public class SQLiteDatabase implements IDatabase {
             "TAG_NAME TEXT              NOT NULL, " +
             "TAG_DESC TEXT              NOT NULL" +
             ")";
-    private final String tableType= "CREATE TABLE IF NOT EXISTS TYPE " +
+    private final String tableType = "CREATE TABLE IF NOT EXISTS TYPE " +
             "(TYPE_ID INT PRIMARY KEY   NOT NULL, " +
             "TYPE_NAME TEXT              NOT NULL, " +
             "TYPE_DESC TEXT              NOT NULL" +
             ")";
-    private final String tableAccount= "CREATE TABLE IF NOT EXISTS ACCOUNT, " +
+    private final String tableAccount = "CREATE TABLE IF NOT EXISTS ACCOUNT, " +
             "(ACCOUNT_ID INT PRIMARY KEY NOT NULL, " +
             "ACCOUNT_NAME TEXT           NOT NULL, " +
             "ACCOUNT_DESC TEXT           NOT NULL" +
             ")";
-    private final String tableAccountBalance= "CREATE TABLE IF NOT EXISTS ACCOUNT_BALANCE " +
+    private final String tableAccountBalance = "CREATE TABLE IF NOT EXISTS ACCOUNT_BALANCE " +
             "(ABAL_ACCOUNT_ID INT       NOT NULL, " +
             "ABAL_DATETIME REAL         NOT NULL, " +
             "ABAL_AMOUNT INT            NOT NULL, " +
             "FOREIGN KEY(TTPE_TAG_ID) REFERENCES TAG(TAG_ID), " +
             "FOREIGN KEY(TTPE_PAYEE_ID) REFERENCES PAYEE(PAYEE_ID)" +
             ")";
-    private final String tablePayee= "CREATE TABLE IF NOT EXISTS PAYEE " +
+    private final String tablePayee = "CREATE TABLE IF NOT EXISTS PAYEE " +
             "(PAYEE_ID INT PRIMARY KEY  NOT NULL, " +
             "PAYEE_NAME TEXT           NOT NULL, " +
             "PAYEE_DESC TEXT           NOT NULL" +
             ")";
-    private final String tableTagToPayee= "CREATE TABLE IF NOT EXISTS TAG_TO_PAYEE " +
+    private final String tableTagToPayee = "CREATE TABLE IF NOT EXISTS TAG_TO_PAYEE " +
             "(TTPE_TAG_ID INT           NOT NULL, " +
             "TTPE_PAYEE_ID INT          NOT NULL, " +
             "FOREIGN KEY(TTPE_TAG_ID) REFERENCES TAG(TAG_ID), " +
             "FOREIGN KEY(TTPE_PAYEE_ID) REFERENCES PAYEE(PAYEE_ID)" +
             ")";
-
 
 
     public void shutdown() {
@@ -123,29 +126,38 @@ public class SQLiteDatabase implements IDatabase {
         }
     }
 
+    // TODO: Review logic and implement helper methods
     public void insertTransaction(Transaction transaction) {
         try {
-            PreparedStatement stmt = database.prepareStatement("INSERT INTO TRANSACTION (TRANS_DATETIME,TRANS_AMOUNT,TRANS_PENDING,TRANS_ACCOUNT_ID,TRANS_PAYEE_ID) VALUES (?, ?, ?, ?, ?)");
+            PreparedStatement stmt = database.prepareStatement("INSERT INTO TRANSACTION (TRANS_DATETIME,TRANS_AMOUNT,TRANS_TYPE_ID,TRANS_PENDING,TRANS_ACCOUNT_ID,TRANS_PAYEE_ID) VALUES (?, ?, ?, ?, ?, ?)");
             stmt.setLong(1, transaction.getDate().getTime());
             stmt.setInt(2, transaction.getAmount());
-            stmt.setBoolean(3, transaction.isPending());
+
+            Type existingType = getTypeForName(transaction.getType().getName());
+            if (existingType != null) {
+                stmt.setInt(3, existingType.getId());
+            } else {
+                System.out.println("ERROR: No matching transaction type found.");
+            }
+
+            stmt.setBoolean(4, transaction.isPending());
 
             Account existingAccount = getAccountForName(transaction.getAccount().getName());
             if (existingAccount != null) {
-                stmt.setInt(4, existingAccount.getId());
+                stmt.setInt(5, existingAccount.getId());
             } else {
                 insertAccount(transaction.getAccount());
                 Account insertedAccount = getAccountForName(transaction.getAccount().getName());
-                stmt.setInt(4, insertedAccount.getId());
+                stmt.setInt(5, insertedAccount.getId());
             }
 
             Payee existingPayee = getPayeeForNameAndDescription(transaction.getPayee().getName(), transaction.getPayee().getDescription());
             if (existingPayee != null) {
-                stmt.setInt(5, existingPayee.getId());
+                stmt.setInt(6, existingPayee.getId());
             } else {
                 insertPayee(transaction.getPayee());
                 Payee insertedPayee = getPayeeForNameAndDescription(transaction.getPayee().getName(), transaction.getPayee().getDescription());
-                stmt.setInt(5, insertedPayee.getId());
+                stmt.setInt(6, insertedPayee.getId());
             }
 
             stmt.executeUpdate();
@@ -198,56 +210,76 @@ public class SQLiteDatabase implements IDatabase {
         }
     }
 
+    // TODO: Review logic and implement helper methods
     public void editTransaction(Transaction transaction) {
         try {
-            PreparedStatement stmt = database.prepareStatement("UPDATE TRANSACTION SET TRANS_DATETIME=?,TRANS_AMOUNT=?,TRANS_PENDING=?,TRANS_ACCOUNT_ID=?,TRANS_PAYEE_ID=? WHERE TRANS_ID=?");
+            PreparedStatement stmt = database.prepareStatement("UPDATE TRANSACTION SET TRANS_DATETIME=?,TRANS_AMOUNT=?,TRANS_TYPE_ID=?,TRANS_PENDING=?,TRANS_ACCOUNT_ID=?,TRANS_PAYEE_ID=? WHERE TRANS_ID=?");
             stmt.setLong(1, transaction.getDate().getTime());
             stmt.setInt(2, transaction.getAmount());
-            stmt.setBoolean(3, transaction.isPending());
+
+            Type existingType = getTypeForName(transaction.getType().getName());
+            if (existingType != null) {
+                stmt.setInt(3, existingType.getId());
+            } else {
+                system.out.println("ERROR: No matching transaction type found.");
+            }
+
+            stmt.setBoolean(4, transaction.isPending());
 
             Account existingAccount = getAccountForName(transaction.getAccount().getName());
             if (existingAccount != null) {
-                stmt.setInt(4, existingAccount.getId());
+                stmt.setInt(5, existingAccount.getId());
             } else {
                 insertAccount(transaction.getAccount());
                 Account insertedAccount = getAccountForName(transaction.getAccount().getName());
-                stmt.setInt(4, insertedAccount.getId());
+                stmt.setInt(5, insertedAccount.getId());
             }
 
             Payee existingPayee = getPayeeForNameAndDescription(transaction.getPayee().getName(), transaction.getPayee().getDescription());
             if (existingPayee != null) {
-                stmt.setInt(5, existingPayee.getId());
+                stmt.setInt(6, existingPayee.getId());
             } else {
                 insertPayee(transaction.getPayee());
                 Payee insertedPayee = getPayeeForNameAndDescription(transaction.getPayee().getName(), transaction.getPayee().getDescription());
-                stmt.setInt(5, insertedPayee.getId());
+                stmt.setInt(6, insertedPayee.getId());
             }
 
-            stmt.setInt(6, transaction.getId());
+            stmt.setInt(7, transaction.getId());
 
             stmt.executeUpdate();
 
-            ResultSet generatedIDs = stmt.getGeneratedKeys();
-            if (generatedIDs.next()) {
-                int insertedTransactionID = generatedIDs.getInt("TRANS_ID");
-
-                for (Tag currentTag : transaction.getTagList()) {
-                    Tag existingTag = getTagForNameAndDescription(currentTag.getName(), currentTag.getDescription());
-                    if (existingTag != null) {
-                        insertTagToTrans(existingTag.getId(), insertedTransactionID);
-                    } else {
-                        insertTag(currentTag);
-                        Tag insertedTag = getTagForNameAndDescription(currentTag.getName(), currentTag.getDescription());
-                        insertTagToTrans(insertedTag.getId(), insertedTransactionID);
-                    }
+            // Update tag associations
+            deleteAllTagToTransForTransactionID(transaction.getId());
+            for (Tag currentTag : transaction.getTagList()) {
+                Tag existingTag = getTagForNameAndDescription(currentTag.getName(), currentTag.getDescription());
+                if (existingTag != null) {
+                    insertTagToTrans(existingTag.getId(), transaction.getId());
+                } else {
+                    insertTag(currentTag);
+                    Tag insertedTag = getTagForNameAndDescription(currentTag.getName(), currentTag.getDescription());
+                    insertTagToTrans(insertedTag.getId(), transaction.getId());
                 }
             }
+
+            // Update transaction note
+            Note updatedNote = transaction.getNote();
+            if (updatedNote != null) {
+                Note existingNote = getNoteForTransactionID(transaction.getId());
+                existingNote.setNoteText(updatedNote.getNoteText());
+                editNote(existingNote);
+            } else {
+                deleteNoteForTransactionID(transaction.getId());
+            }
+
+            // Close SQL statement
+            stmt.close();
+
         } catch (java.sql.SQLException e) {
             e.printStackTrace();
         }
     }
 
-    // TODO: Fix pls
+    // TODO: Review logic and implement helper methods
     public List<Transaction> getAllTransactions() {
         try {
             Statement stmt = database.createStatement();
@@ -255,21 +287,30 @@ public class SQLiteDatabase implements IDatabase {
 
             ArrayList<Transaction> transactionList = new ArrayList<Transaction>();
 
-            while ( rs.next() ) {
+            while (rs.next()) {
                 Date date = new Date(rs.getLong("TRANS_DATETIME"));
                 int transactionID = rs.getInt("TRANS_ID");
+                int typeID = rs.getInt("TRANS_TYPE_ID");
                 int amount = rs.getInt("TRANS_AMOUNT");
                 boolean pending = rs.getBoolean("TRANS_PENDING");
                 int accountID = rs.getInt("TRANS_ACCOUNT_ID");
                 int payeeID = rs.getInt("TRANS_PAYEE_ID");
 
-                Account account = getAccountFor(accountID);
+                Type type = getTypeForID(typeID)
+                Account account = getAccountForID(accountID);
                 Payee payee = getPayeeForID(payeeID);
                 List<Tag> tags = getTagsForTransactionID(transactionID);
                 Note note = getNoteForTransactionID(transactionID);
 
                 Transaction currentTransaction = new Transaction(date, type, amount, account, payee, pending, transactionID, tags, note);
+                transactionList.add(currentTransaction);
             }
+
+            // Close SQLite statements and result sets
+            stmt.close();
+            rs.close();
+
+            return transactionList;
         } catch (java.sql.SQLException e) {
             e.printStackTrace();
         }
