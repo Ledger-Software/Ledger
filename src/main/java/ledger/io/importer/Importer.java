a8b2dbb677919ef615e1d111fc84d28d1d495605
Tayler How <taylerhow@gmail.com>
2016-10-11 16:07:57 -0400

    [LED-46] Created Importer singleton class to handle importing lists of Java object Transactions into the DB

---
+++ src/main/java/ledger/io/importer/Importer.java
@@ -0,0 +1,64 @@
+package ledger.io.importer;
+
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.Import;
+import ledger.database.IDatabase;
+import ledger.database.enity.Transaction;
+import ledger.exception.StorageException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by Tayler How on 10/11/2016.
+ *
+ * This singleton class is responsible for taking lists of Java Transaction objects
+ * and importing them into the backend DB.
+ */
+public class Importer {
+    private static Importer instance;
+    private List<Transaction> failedImports;
+    private List<String> failedImportExceptionMessages;
+
+    public Importer() {
+        this.failedImports = new ArrayList<>();
+        this.failedImportExceptionMessages = new ArrayList<>();
+    }
+    
+    public static Importer getInstance() {
+        if (instance != null) {
+            return instance;
+        } else {
+            instance = new Importer();
+            return instance;
+        }
+    }
+
+    public boolean importTransactions(IDatabase database, List<Transaction> transactionList) {
+        this.failedImports.clear();
+        this.failedImportExceptionMessages.clear();
+
+        for (Transaction currentTransaction : transactionList) {
+            try {
+                database.insertTransaction(currentTransaction);
+            } catch (StorageException e) {
+                this.failedImports.add(currentTransaction);
+                this.failedImportExceptionMessages.add(e.getMessage());
+            }
+        }
+
+        if (this.failedImports.size() == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public List<Transaction> getFailedImports() {
+        return this.failedImports;
+    }
+
+    public List<String> getFailedImportExceptionMessages() {
+        return this.failedImportExceptionMessages;
+    }
+}
