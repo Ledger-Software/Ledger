69ddadd2b34e0c08f983480d246828a7185d52f7
Emily Richardson <richarel@rose-hulman.edu>
2016-10-11 22:03:17 -0400

    [LED-48] messing with modals

--- src/main/java/ledger/user_interface/Startup.java
+++ src/main/java/ledger/user_interface/Startup.java
@@ -1,7 +1,15 @@
 package ledger.user_interface;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  * Handles any tasks relevant for init of the Program.
@@ -19,6 +27,18 @@ public class Startup extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setAlwaysOnTop(true); // Development Settings
         primaryStage.setTitle("Ledger");
+        testPopup(primaryStage);
         primaryStage.show();
+    }
+
+    private void testPopup(Stage primaryStage){
+        final Stage dialog = new Stage();
+        dialog.initOwner(primaryStage);
+        dialog.initModality(Modality.WINDOW_MODAL);
+        VBox dialogVBox = new VBox(20);
+        Scene dialogScene = new Scene(dialogVBox, 300, 200);
+        dialogVBox.getChildren().addAll(new Text("Some stuff"));
+        dialog.setScene(dialogScene);
+        dialog.show();
     }
 }
