1a3caf9d8c7ac3d83244444d5f4fc0b9b5087117
Jesse Shellabarger <shellajt@rose-hulman.edu>
2016-10-12 00:51:26 -0400

    [LED-46] Tested converter for qfx

--- src/main/java/ledger/io/input/QfxConverter.java
+++ src/main/java/ledger/io/input/QfxConverter.java
@@ -2,7 +2,6 @@ package ledger.io.input;
 
 import ledger.database.enity.*;
 import org.w3c.dom.Document;
-import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -13,6 +12,7 @@ import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringReader;
+import java.time.OffsetDateTime;
 import java.util.*;
 import java.util.regex.Pattern;
 
@@ -43,13 +43,10 @@ public class QfxConverter implements IInAdapter {
         int lastIndexOfTrans = sgml.indexOf("</BANKTRANLIST>");
         sgml = sgml.substring(0, lastIndexOfTrans);
 
-        System.out.println("After removing metadata: " + sgml);
-
         // add tags to the all rows that are not STMTTRN
         String[] splitPieces = sgml.split("<");
         LinkedList<String> modifiedPieces = new LinkedList();
         for (String piece : splitPieces) {
-            System.out.println(piece);
             boolean matches = Pattern.matches("(?!STMTTRN[>].*|[/]STMTTRN[>].*).*[>].*", piece);
             if (matches) {
                 int lastClosingTag = piece.indexOf(">");
@@ -67,9 +64,6 @@ public class QfxConverter implements IInAdapter {
             correctedXml.append(piece);
         }
         correctedXml.append("\n" + "</QFX>");
-
-        System.out.println("After stitching: " + correctedXml);
-
 
         // XML Parse
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -98,15 +92,14 @@ public class QfxConverter implements IInAdapter {
 
         // pull out relevant data and create java objects
         for (int i = 0; i < transactionTypes.getLength(); i++) {
-            // yyyy-MM-dd'T'HH:mm:ss.SSSZ`  2001-07-04T12:08:56.235-0700
-            Date date = new Date(extractDate(transactionDates.item(i).getNodeValue()));
+            Date date = new Date(GenerateEpoch.generate(transactionDates.item(i).getTextContent()));
             //TODO: Discuss what to do about type
             Type type = new Type("Error","No type exists");
-            int amount = (int) (Double.parseDouble((transactionAmounts.item(i).getNodeValue())) * 100);
-            Payee payee = new Payee(names.item(i).getNodeValue(), "");
+            int amount =  (int) ((long) (Math.floor((Double.parseDouble((transactionAmounts.item(i).getTextContent())) * 100) + 0.5d)));
+            Payee payee = new Payee(names.item(i).getTextContent(), "");
             //TODO: Discuss what to do about tags
             List<Tag> tags = new LinkedList<>();
-            Note note = new Note(memos.item(i).getNodeValue());
+            Note note = new Note(memos.item(i).getTextContent());
 
             //TODO: Discuss what to do about pending
 
@@ -116,44 +109,5 @@ public class QfxConverter implements IInAdapter {
 
         // return list
         return transactions;
-    }
-
-    private String extractDate(String xmlDate) {
-        StringBuilder extractedDate = new StringBuilder();
-
-        int year = Integer.parseInt(xmlDate.substring(0, 4));
-        int month = Integer.parseInt(xmlDate.substring(4, 6));
-        int day = Integer.parseInt(xmlDate.substring(6, 8));
-
-        int hours = Integer.parseInt(xmlDate.substring(8, 10));
-        int indexOfOpenBracket = xmlDate.indexOf("[");
-        int indexOfColon = xmlDate.indexOf(":");
-        int timezoneOffset = Integer.parseInt(xmlDate.substring(indexOfOpenBracket + 1, indexOfColon));
-        int utcHours = hours - timezoneOffset;
-        if (utcHours < 0) {
-            utcHours += 24;
-            day -= 1;
-        } else if (utcHours > 24) {
-            utcHours -= 24;
-            day += 1;
-        }
-
-        int minutes = Integer.parseInt(xmlDate.substring(10, 12));
-        int seconds = Integer.parseInt(xmlDate.substring(12, 14));
-
-        extractedDate.append(year);
-        extractedDate.append("-");
-        extractedDate.append(month);
-        extractedDate.append("-");
-        extractedDate.append(day);
-        extractedDate.append("T");
-        extractedDate.append(utcHours);
-        extractedDate.append(":");
-        extractedDate.append(minutes);
-        extractedDate.append(":");
-        extractedDate.append(seconds);
-        extractedDate.append(".000-0000");
-
-        return extractedDate.toString();
     }
 }
--- src/test/java/ledger/io/input/QfxConverterTest.java
+++ src/test/java/ledger/io/input/QfxConverterTest.java
@@ -1,24 +1,55 @@
 package ledger.io.input;
 
 import ledger.database.enity.Account;
+import ledger.database.enity.Transaction;
+import org.junit.Test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by Jesse Shellabarger on 10/11/2016.
  */
 public class QfxConverterTest {
-    public static void main(String[] args) throws FileNotFoundException {
+
+    @Test
+    public void importTransactionsTest() throws FileNotFoundException {
         File testFile = new File("src/test/resources/testQfx.QFX");
         IInAdapter adapter = new QfxConverter(testFile, new Account("test", "test"));
 
+        List<Transaction> convertedTransactions = null;
         try {
-            adapter.convert();
+            convertedTransactions = adapter.convert();
         } catch (IOException e) {
             e.printStackTrace();
+            fail();
         }
+
+        assertEquals(4, convertedTransactions.size());
+
+        assertEquals(-3309, convertedTransactions.get(0).getAmount());
+        assertEquals(-2031, convertedTransactions.get(1).getAmount());
+        assertEquals(-499, convertedTransactions.get(2).getAmount());
+        assertEquals(-699, convertedTransactions.get(3).getAmount());
+
+        assertEquals(new Date(GenerateEpoch.generate("20160912000000[-4:EDT]")), convertedTransactions.get(0).getDate());
+        assertEquals(new Date(GenerateEpoch.generate("20160912000000[-4:EDT]")), convertedTransactions.get(1).getDate());
+        assertEquals(new Date(GenerateEpoch.generate("20160912000000[-4:EDT]")), convertedTransactions.get(2).getDate());
+        assertEquals(new Date(GenerateEpoch.generate("20160919000000[-4:EDT]")), convertedTransactions.get(3).getDate());
+
+        assertEquals("OUTBACK 1515", convertedTransactions.get(0).getPayee().getName());
+        assertEquals("BIG RED LIQUORS - 011", convertedTransactions.get(1).getPayee().getName());
+        assertEquals("Spotify USA", convertedTransactions.get(2).getPayee().getName());
+        assertEquals("WHITE CASTLE  050057", convertedTransactions.get(3).getPayee().getName());
+
+        assertEquals("TERRE HAUTE  IN", convertedTransactions.get(0).getNote().getNoteText());
+        assertEquals("TERRE HAUTE  IN", convertedTransactions.get(1).getNote().getNoteText());
+        assertEquals("New York     NY", convertedTransactions.get(2).getNote().getNoteText());
+        assertEquals("TERRE HAUTE  IN", convertedTransactions.get(3).getNote().getNoteText());
     }
 }
