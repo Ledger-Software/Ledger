97ed2d10ad52b6c7fd9ce8c71efc309c55182ac4
Emily Richardson <richarel@rose-hulman.edu>
2016-10-23 15:11:14 -0400

    found a defect in the import transaction popup and fixed it

--- src/main/java/ledger/user_interface/ui_controllers/ImportTransactionsPopupController.java
+++ src/main/java/ledger/user_interface/ui_controllers/ImportTransactionsPopupController.java
@@ -18,6 +18,8 @@ import java.util.ResourceBundle;
 public class ImportTransactionsPopupController extends GridPane implements Initializable, IUIController {
 
     @FXML
+    private Button submitTrxnFileBtn;
+    @FXML
     private Button chooseTrxnFileBtn;
     @FXML
     private ChoiceBox fileExtChooser;
