09c810106601126f133ab70388becd616f7035fa
Gert <xxshadowfang@gmail.com>
2016-10-26 14:53:07 -0400

    Fix MainPage imports

--- src/main/java/ledger/user_interface/ui_controllers/MainPageController.java
+++ src/main/java/ledger/user_interface/ui_controllers/MainPageController.java
@@ -1,10 +1,13 @@
 package ledger.user_interface.ui_controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.net.URL;
 import java.util.ResourceBundle;
