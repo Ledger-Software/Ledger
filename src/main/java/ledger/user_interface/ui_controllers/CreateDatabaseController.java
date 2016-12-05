cbd2a969b2b316dd6dad752e6c6f3ffd064eec85
CJ Miller <cjmiller721@gmail.com>
2016-12-05 13:24:24 -0500

    [LED-108] Using File Chooser now. It will ask if you want to replace the file. One you hit Ok it will Delete the old file if it exists

--- src/main/java/ledger/user_interface/ui_controllers/CreateDatabaseController.java
+++ src/main/java/ledger/user_interface/ui_controllers/CreateDatabaseController.java
@@ -6,25 +6,29 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import ledger.controller.DbController;
 import ledger.exception.StorageException;
 import ledger.user_interface.utils.InputSanitization;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class CreateDatabaseController extends Pane implements IUIController, Initializable {
-
-    @FXML
-    private TextField fileName;
+public class CreateDatabaseController extends GridPane implements IUIController, Initializable {
     @FXML
     private TextField password;
     @FXML
     private TextField confirmPassword;
     @FXML
     private Button submitButton;
+    @FXML
+    private Button saveLocationButton;
+
+    private File saveLocation;
 
     @FXML
     public void onEnter(ActionEvent ae) {
@@ -33,6 +37,8 @@ public class CreateDatabaseController extends Pane implements IUIController, Ini
 
     public final static String pageLoc = "/fxml_files/CreateDatabasePage.fxml";
 
+
+
     CreateDatabaseController() {
         this.initController(pageLoc, this, "Error on new database startup: ");
     }
@@ -40,11 +46,26 @@ public class CreateDatabaseController extends Pane implements IUIController, Ini
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         this.submitButton.setOnAction(event -> submitForm());
+        this.saveLocationButton.setOnAction(this::saveLocation);
+    }
+
+    private void saveLocation(ActionEvent actionEvent) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setInitialFileName("LedgerDB");
+        fileChooser.setTitle("Select Save File");
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Ledger files (*.mv.db)", "*.mv.db");
+        fileChooser.getExtensionFilters().add(extFilter);
+        fileChooser.setSelectedExtensionFilter(extFilter);
+        fileChooser.setInitialDirectory(new File(System.getProperty("user.home")));
+        File file = fileChooser.showSaveDialog(this.getScene().getWindow());
+        if(file != null) {
+            this.saveLocationButton.setText(file.getName());
+            this.saveLocation = file;
+        }
     }
 
     private void submitForm() {
-        String fileName = this.fileName.getText();
-        if (InputSanitization.isStringInvalid(fileName)) {
+        if (this.saveLocation == null) {
             setupErrorPopup("Please provide a file name", new Exception());
             return;
         }
@@ -58,7 +79,10 @@ public class CreateDatabaseController extends Pane implements IUIController, Ini
         }
 
         try {
-            DbController.INSTANCE.initialize("~/" + this.fileName.getText(), this.password.getText());
+            if(this.saveLocation.exists())
+                this.saveLocation.delete();
+
+            DbController.INSTANCE.initialize(saveLocation.getAbsolutePath(), this.password.getText());
 
             Startup.INSTANCE.switchScene(new Scene(new MainPageController()), "Ledger");
 
