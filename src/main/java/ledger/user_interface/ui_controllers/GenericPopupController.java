1819a44037daa8520064d36c5bf426d3f198c8c9
CJ Miller <cjmiller721@gmail.com>
2016-12-08 03:26:40 -0500

    [LED-113] Refactoring UI Code

--- src/main/java/ledger/user_interface/ui_controllers/GenericPopupController.java
+++ src/main/java/ledger/user_interface/ui_controllers/GenericPopupController.java
@@ -16,8 +16,9 @@ import java.util.ResourceBundle;
  * fit whatever purpose we need
  */
 public class GenericPopupController extends GridPane implements Initializable, IUIController {
+
     @FXML
-    private Label errorText;
+    private Text errorText;
     @FXML
     private Button okayBtn;
     @FXML
