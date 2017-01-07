0557ffb79dbae8d88fcc3150cdcfbdd2907f8961
Emily Richardson <richarel@rose-hulman.edu>
2017-01-07 12:41:58 -0500

    set up and stubbed out controller class

--- src/main/java/ledger/user_interface/ui_controllers/ExpenditureChartsController.java
+++ src/main/java/ledger/user_interface/ui_controllers/ExpenditureChartsController.java
@@ -8,8 +8,11 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import ledger.database.entity.Account;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 /**
@@ -39,7 +42,7 @@ public class ExpenditureChartsController extends GridPane implements Initializab
     }
 
     /**
-     * Will be used to set up the charts on this page
+     * Is used to set up the charts on this page
      * <p>
      * Called to initialize a controller after its root element has been
      * completely processed.
@@ -53,8 +56,79 @@ public class ExpenditureChartsController extends GridPane implements Initializab
     public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
 
         this.filterEnterButton.setOnAction((event) -> {
+            getTransactions();
+            setupExpenditureHistoryChart();
+
+            Account accountSelected = accountFilterDropdown.getSelectedAccount();
+            LocalDate fromDateSelected = fromDateFilter.getValue();
+            LocalDate toDateSelected = toDateFilter.getValue();
+            if (accountSelected.equals(null) && fromDateSelected.equals(null) && toDateSelected.equals(null)) {
+                this.setupErrorPopup("You must either select an Account or a date range to continue!", new Exception());
+                return;
+            }
+            if (!accountSelected.equals(null) && (fromDateSelected.equals(null) || toDateSelected.equals(null))) {
+                createBasedOnAccount(accountSelected);
+            }
+            if (accountSelected.equals(null) && !(fromDateSelected.equals(null) && toDateSelected.equals(null))) {
+                if (fromDateSelected.isAfter(toDateSelected)) {
+                    this.setupErrorPopup("Ensure your dates are in chronological order!", new Exception());
+                    return;
+                }
+                createBasedOnDateRange(fromDateSelected, toDateSelected);
+            }
+            if (!(accountSelected.equals(null) && fromDateSelected.equals(null) && toDateSelected.equals(null))) {
+                if (fromDateSelected.isAfter(toDateSelected)) {
+                    this.setupErrorPopup("Ensure your dates are in chronological order!", new Exception());
+                    return;
+                }
+                createBasedOnAccountAndDateRange(accountSelected, fromDateSelected, toDateSelected);
+            }
 
         });
+
+    }
+
+    /**
+     * Retrieves transactions from the database and sets field for use
+     */
+    private void getTransactions() {
+
+    }
+
+    /**
+     * Builds the line chart to show trends in amount spent over the last six months
+     */
+    private void setupExpenditureHistoryChart() {
+
+    }
+
+    /**
+     * Creates the stacked bar chart based on transactions in a specified account
+     *
+     * @param account the account to filter the transactions by
+     */
+    private void createBasedOnAccount(Account account) {
+
+    }
+
+    /**
+     * Creates the stacked bar chart based on transactions in a specified date range
+     *
+     * @param fromDate starting date to filter transactions by
+     * @param toDate ending date to filter transactions by
+     */
+    private void createBasedOnDateRange(LocalDate fromDate, LocalDate toDate) {
+
+    }
+
+    /**
+     * Creates the stacked bar chart based the transactions that exist in a specified account within a given date range
+     *
+     * @param account the account to filter the transactions by
+     * @param fromDate starting date to filter transactions by
+     * @param toDate ending date to filter transactions by
+     */
+    private void createBasedOnAccountAndDateRange(Account account, LocalDate fromDate, LocalDate toDate) {
 
     }
 }
