b7f0949858dc16c836b4c69ab02d23f962e1af21
Jesse Shellabarger <shellajt@rose-hulman.edu>
2017-01-13 12:07:01 -0500

    Removed some files and directories we dont use

--- src/main/java/ledger/database/manager/TransactionManager.java
+++
@@ -1,9 +0,0 @@
-package ledger.database.manager;
-
-import ledger.database.entity.Transaction;
-
-/**
- * Manages Transaction Entities
- */
-public class TransactionManager implements ITimeSeries<Transaction> {
-}
