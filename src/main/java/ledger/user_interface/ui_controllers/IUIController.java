93d3229f7e53bec6881c683228216c4519e8fddf
Jesse Shellabarger <shellajt@rose-hulman.edu>
2017-01-13 12:21:51 -0500

    [LED-133] Fix for empty exceptions

--- src/main/java/ledger/user_interface/ui_controllers/IUIController.java
+++ src/main/java/ledger/user_interface/ui_controllers/IUIController.java
@@ -34,31 +34,33 @@ public interface IUIController {
             alert.setContentText(e.getMessage());
         }
 
-        // Create expandable Exception.
-        StringWriter sw = new StringWriter();
-        PrintWriter pw = new PrintWriter(sw);
+        if (e.getStackTrace() != null) {
+            // Create expandable Exception.
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
 
-        e.printStackTrace(pw);
-        String exceptionText = sw.toString();
+            e.printStackTrace(pw);
+            String exceptionText = sw.toString();
 
-        Label label = new Label("The exception stacktrace was:");
+            Label label = new Label("The exception stacktrace was:");
 
-        TextArea textArea = new TextArea(exceptionText);
-        textArea.setEditable(false);
-        textArea.setWrapText(true);
+            TextArea textArea = new TextArea(exceptionText);
+            textArea.setEditable(false);
+            textArea.setWrapText(true);
 
-        textArea.setMaxWidth(Double.MAX_VALUE);
-        textArea.setMaxHeight(Double.MAX_VALUE);
-        GridPane.setVgrow(textArea, Priority.ALWAYS);
-        GridPane.setHgrow(textArea, Priority.ALWAYS);
+            textArea.setMaxWidth(Double.MAX_VALUE);
+            textArea.setMaxHeight(Double.MAX_VALUE);
+            GridPane.setVgrow(textArea, Priority.ALWAYS);
+            GridPane.setHgrow(textArea, Priority.ALWAYS);
 
-        GridPane expContent = new GridPane();
-        expContent.setMaxWidth(Double.MAX_VALUE);
-        expContent.add(label, 0, 0);
-        expContent.add(textArea, 0, 1);
+            GridPane expContent = new GridPane();
+            expContent.setMaxWidth(Double.MAX_VALUE);
+            expContent.add(label, 0, 0);
+            expContent.add(textArea, 0, 1);
 
-        // Set expandable Exception into the dialog pane.
-        alert.getDialogPane().setExpandableContent(expContent);
+            // Set expandable Exception into the dialog pane.
+            alert.getDialogPane().setExpandableContent(expContent);
+        }
 
         alert.showAndWait();
     }
