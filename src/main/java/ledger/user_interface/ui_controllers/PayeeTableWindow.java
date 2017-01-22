f9a98044dbe5222c06468a15fb1604231a3be2f2
CJ Miller <cjmiller721@gmail.com>
2017-01-22 03:56:30 -0500

    [LED-109] Getting a weird load exception, but everything is loading fine

--- src/main/java/ledger/user_interface/ui_controllers/PayeeTableWindow.java
+++ src/main/java/ledger/user_interface/ui_controllers/PayeeTableWindow.java
@@ -10,10 +10,10 @@ import java.util.ResourceBundle;
  * Created by CJ on 1/22/2017.
  */
 public class PayeeTableWindow extends GridPane implements IUIController, Initializable {
-    private static final String pageLog = "/fxml_files/PayeeTableWindow.fxml";
+    private static final String pageLoc = "/fxml_files/Temp.fxml";
 
     public PayeeTableWindow() {
-        this.initController(pageLog, this, "Unable to load Payee editor window");
+        this.initController(pageLoc, this, "Unable to load Payee editor window");
     }
 
     @Override
