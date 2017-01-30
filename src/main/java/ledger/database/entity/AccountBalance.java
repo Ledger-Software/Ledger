576226159ea1a15ce6d1e8a17da2df96884bdb7f
Jesse Shellabarger <shellajt@rose-hulman.edu>
2017-01-29 22:52:04 -0500

    [LED-142] Added DB backend for account balances

--- src/main/java/ledger/database/entity/AccountBalance.java
+++ src/main/java/ledger/database/entity/AccountBalance.java
@@ -9,6 +9,7 @@ public class AccountBalance implements IEntity {
     private Account account;
     private Date date;
     private int amount;
+    private int id;
 
     public AccountBalance(Account account, Date date, int amount) {
         this.account = account;
@@ -16,19 +17,54 @@ public class AccountBalance implements IEntity {
         this.amount = amount;
     }
 
+    public AccountBalance(Account account, Date date, int amount, int id) {
+        this.account = account;
+
+        this.date = date;
+        this.amount = amount;
+        this.id = id;
+    }
+
     /**
-     * Gets the AccountBalance ID
+     * Get the AccountBalance's database ID
      *
-     * @return id
+     * @return database id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * Set the AccountBalance's database ID
+     *
+     * @param id database id
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * Gets the Account this Balance is associated with
+     *
+     * @return id The account associate with this balance
      */
     public Account getAccount() {
         return account;
     }
 
     /**
+     * Sets the account associated with this balance
+     *
+     * @param account the account to assocate the balance with
+     */
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    /**
      * Gets the AccountBalance date
      *
-     * @return date
+     * @return date the existing date
      */
     public Date getDate() {
         return date;
@@ -46,7 +82,7 @@ public class AccountBalance implements IEntity {
     /**
      * Gets the AccountBalance amount
      *
-     * @return amount
+     * @return amount the ammount of the current balance
      */
     public int getAmount() {
         return amount;
