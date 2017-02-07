370bd65568c95e7a2cd8bd6fc8e60456e710ab34
Jesse Shellabarger <shellajt@rose-hulman.edu>
2017-02-06 22:45:34 -0500

    Enabled selection of accounts from table view

--- src/main/java/ledger/user_interface/ui_controllers/component/tablecolumn/AccountColumn.java
+++ src/main/java/ledger/user_interface/ui_controllers/component/tablecolumn/AccountColumn.java
@@ -1,9 +1,16 @@
 package ledger.user_interface.ui_controllers.component.tablecolumn;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.cell.ComboBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import ledger.controller.DbController;
+import ledger.controller.register.TaskWithArgs;
+import ledger.controller.register.TaskWithReturn;
 import ledger.database.entity.Account;
 import ledger.database.entity.Transaction;
 import ledger.user_interface.ui_controllers.IUIController;
@@ -12,6 +19,7 @@ import ledger.user_interface.utils.AccountStringConverter;
 import ledger.user_interface.utils.PayeeStringConverter;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 /**
@@ -26,9 +34,39 @@ public class AccountColumn extends TableColumn implements IUIController, Initial
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        TaskWithReturn<List<Account>> getAllAccountTask = DbController.INSTANCE.getAllAccounts();
+        getAllAccountTask.startTask();
+        List<Account> allAccounts = getAllAccountTask.waitForResult();
+        ObservableList observableAllAccounts = FXCollections.observableList(allAccounts);
+
+
         this.setCellValueFactory(new PropertyValueFactory<Transaction, Account>("account"));
-        this.setCellFactory(TextFieldTableCell.forTableColumn(new AccountStringConverter()));
+        this.setCellFactory(ComboBoxTableCell.forTableColumn(new AccountStringConverter(), observableAllAccounts));
+        this.setOnEditCommit(this.accountEditHandler);
         this.setComparator(new AccountComparator());
-        this.setEditable(false);
+
+        for (Account currentAccount : allAccounts) {
+            if (!observableAllAccounts.contains(currentAccount)) {
+                observableAllAccounts.add(currentAccount);
+            }
+        }
     }
+
+    private EventHandler<javafx.scene.control.TableColumn.CellEditEvent<Transaction, Account>> accountEditHandler = new EventHandler<javafx.scene.control.TableColumn.CellEditEvent<Transaction, Account>>() {
+        @Override
+        public void handle(javafx.scene.control.TableColumn.CellEditEvent<Transaction, Account> t) {
+            Transaction transaction = t.getTableView().getItems().get(t.getTablePosition().getRow());
+            Account accountToSet = t.getNewValue();
+
+            transaction.setAccount(accountToSet);
+
+            TaskWithArgs<Transaction> task = DbController.INSTANCE.editTransaction(transaction);
+            task.RegisterFailureEvent((e) -> {
+                setupErrorPopup("Error editing transaction payee.", e);
+            });
+
+            task.startTask();
+            task.waitForComplete();
+        }
+    };
 }
