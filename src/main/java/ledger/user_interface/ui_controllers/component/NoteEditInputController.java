89a574c2e12b3f31d1cf6a4af8eecb882c7f9184
Garrett Barnes <xxshadowfang@gmail.com>
2017-02-10 19:16:03 -0500

    [LED-159] F notes ui fixes (#178)
    * Fix- Sizing, Column, extra code
    
    * Fixed tag focus causing spacing issues
    
    * Button doesnt float
    
    * Fixed horizontal sizing
    
    * Removed save button, Enter now commits, shift-enter makes a new-line

--- src/main/java/ledger/user_interface/ui_controllers/component/NoteEditInputController.java
+++ src/main/java/ledger/user_interface/ui_controllers/component/NoteEditInputController.java
@@ -1,12 +1,17 @@
 package ledger.user_interface.ui_controllers.component;
 
 
+import javafx.beans.InvalidationListener;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import ledger.controller.DbController;
@@ -25,16 +30,12 @@ import java.util.ResourceBundle;
  */
 public class NoteEditInputController extends GridPane implements IUIController, Initializable {
     private static final String pageLoc = "/fxml_files/NoteEditInput.fxml";
-    @FXML
-    private TextField compactNoteText;
+
     @FXML
     private TextArea noteText;
-    @FXML
-    private Button saveButton;
-    @FXML
-    private Button cancelButton;
+
+
     private Transaction transaction;
-    private CallableMethod<Boolean> collapseMethod;
 
     /**
      * Basic Constructor
@@ -46,42 +47,52 @@ public class NoteEditInputController extends GridPane implements IUIController, 
         }
         this.initController(pageLoc, this, "Unable to Load Note Editor");
 
+
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        this.compactNoteText.setText(this.transaction.getNote().getNoteText());
-        toggleEditors(false);
-        this.
-                saveButton.setOnAction((event -> {
-            this.transaction.getNote().setNoteText(noteText.getText());
-            TaskWithArgs<Transaction> updateNoteTask;
-            updateNoteTask = DbController.INSTANCE.editTransaction(this.transaction);
-            updateNoteTask.RegisterSuccessEvent(() -> Startup.INSTANCE.runLater(() -> {
-                this.toggleEditors(false);
-                this.compactNoteText.setText(this.transaction.getNote().getNoteText());
-            }));
-            updateNoteTask.startTask();
-            updateNoteTask.waitForComplete();
-        }));
-        this.cancelButton.setOnAction((event -> {
-            toggleEditors(false);
-        }));
-        this.compactNoteText.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                toggleEditors(true);
-            }
-        });
+        this.toggleEditors(false);
+        this.noteText.setText(this.transaction.getNote().getNoteText());
+        this.noteText.focusedProperty().addListener(new NoteFocusChangeListener());
+        this.noteText.setOnKeyPressed(new NoteKeyEventHandler());
     }
 
-    private void toggleEditors(boolean val) {
-        this.noteText.setText(this.transaction.getNote().getNoteText());
-        this.compactNoteText.setManaged(!val);
-        this.compactNoteText.setVisible(!val);
-        this.noteText.setManaged(val);
-        this.cancelButton.setManaged(val);
-        this.saveButton.setManaged(val);
+    private void toggleEditors(boolean isToggleOpen) {
+        if(isToggleOpen) {
+            this.noteText.setMinHeight(90);
+        } else{
+            this.noteText.setText(transaction.getNote().getNoteText());
+            this.noteText.setMinHeight(30);
+        }
+
+    }
+    private class NoteFocusChangeListener implements ChangeListener<Boolean> {
+
+
+        @Override
+        public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+            toggleEditors(newValue);
+        }
+    }
+    private class NoteKeyEventHandler implements EventHandler<KeyEvent> {
+
+
+        @Override
+        public void handle(KeyEvent event) {
+            if(event.isShiftDown()&&event.getCode()== KeyCode.ENTER){
+                noteText.appendText(System.lineSeparator());
+            } else if (event.getCode()== KeyCode.ENTER){
+                transaction.getNote().setNoteText(noteText.getText());
+                TaskWithArgs<Transaction> updateNoteTask;
+                updateNoteTask = DbController.INSTANCE.editTransaction(transaction);
+                updateNoteTask.RegisterSuccessEvent(() -> Startup.INSTANCE.runLater(() -> {
+                    toggleEditors(false);
+                }));
+                updateNoteTask.startTask();
+                updateNoteTask.waitForComplete();
+            }
+        }
     }
 
 
