2bcf413f9cfc04a3fc4967853f5acc9620b86590
Jesse Shellabarger <shellajt@rose-hulman.edu>
2017-02-12 16:02:36 -0500

    Rephrased error popup to be currency agnostic

--- src/main/java/ledger/user_interface/ui_controllers/window/AccountPopupController.java
+++ src/main/java/ledger/user_interface/ui_controllers/window/AccountPopupController.java
@@ -119,10 +119,10 @@ public class AccountPopupController extends GridPane implements Initializable, I
         Double tmpAmt = Double.parseDouble(accountAmtText.getText()) * 100;
         amount = tmpAmt.longValue();
         if (amount == Long.MAX_VALUE) {
-            this.setupErrorPopup("A maximum of " + Long.MAX_VALUE/100 + " dollars may be inserted an account");
+            this.setupErrorPopup("An account can a maximum balance of " + Long.MAX_VALUE/100);
             return null;
         } else if (amount == Long.MIN_VALUE) {
-            this.setupErrorPopup("A minimum of " + Long.MIN_VALUE/100 + " dollars may be inserted an account");
+            this.setupErrorPopup("An account can a minimum balance of " + Long.MIN_VALUE/100);
             return null;
         }
         ab = new AccountBalance(this.act, new Date(), amount);
