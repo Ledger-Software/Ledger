f699577048fc164027a7ea148f10b20a15258176
Jesse Shellabarger <shellajt@rose-hulman.edu>
2017-03-14 21:19:20 -0400

    [LED-162] Change test to account for new users

--- src/test/java/ledger/user_interface/LoginIntegrationTests.java
+++ src/test/java/ledger/user_interface/LoginIntegrationTests.java
@@ -8,6 +8,7 @@ import ledger.database.entity.Account;
 import ledger.user_interface.ui_controllers.Startup;
 import org.testfx.framework.junit.ApplicationTest;
 
+import java.io.File;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -23,6 +24,25 @@ public class LoginIntegrationTests extends ApplicationTest{
     }
 
     public void createDatabase() {
+        File initDir = new File(System.getProperty("user.home"));
+        File[] listFiles = initDir.listFiles();
+        boolean containsDbFile = false;
+        for (File f : listFiles) {
+            if (f.isFile() && f.getName().endsWith(".mv.db")) {
+                containsDbFile = true;
+            }
+        }
+
+        if (!containsDbFile) {
+            press(KeyCode.ALT);
+            press(KeyCode.TAB);
+
+            release(KeyCode.ALT);
+            release(KeyCode.TAB);
+
+            type(KeyCode.ENTER);
+        }
+
         clickOn("New File");
         clickOn("Select Location");
         type(KeyCode.ENTER);
